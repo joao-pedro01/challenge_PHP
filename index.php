d391ee3ba4662984e11f37c26fdd20fe50e1acac
João Pedro <80638211+joao-pedro01@users.noreply.github.com>
2021-09-11 19:50:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        input
-        {
+        input {
             margin-top:10px;
         }
-        input::placeholder
-        {
+        input::placeholder {
             color: gray;
         }
     </style>
